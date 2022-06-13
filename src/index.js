f8f80ebb663120ca7d335f8a7152b3aeed4dd188
oscar kiprop chemonges <oscar.chemonges@student.moringaschool.com>
2022-06-13 20:26:08 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => { 
+    e.preventDefault()
+    myToDoList(document.getElementById("new-task-description").value)
+  })
+  form.reset()
 });
+
+function myToDoList(todo){
+  const li = document.createElement ('li')
+  const btn = document.createElement ('button')
+  btn.addEventListener('click', deleteTasks)
+  btn.textContent = 'x'
+  li.textContent = `${todo} `
+  li.appendChild(btn)
+  document.querySelector('#tasks').appendChild(li)
+}
+
+function deleteTasks (e){
+e.target.parentNode.remove()
+}
